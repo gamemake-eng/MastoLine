6a3370ba070e34f1af0c6387ee2540ee5006874f
gamemake-eng <mohinathsivaneswaran@gmail.com>
2023-12-09 18:51:59 -0600

    added markdown rendering

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import html2text
 import getpass
 import os.path
 import configparser
+from connect.utils.terminal.markdown import render
 
 
 if os.path.isfile("settings.ini") == False:
+    print("setting up program files")
     f = open("settings.ini", "x")
     f.close()
     config = configparser.ConfigParser()
@@ -23,7 +25,7 @@ if os.path.isfile("./user.secret") == False:
     at = mastodon.log_in(code=code, to_file="user.secret")
     
 h = html2text.HTML2Text()
-
+print("logging on")
 mastodon = Mastodon(access_token="user.secret", api_base_url="https://mstdn.social")
 timeline = mastodon.timeline_home(limit=100)
 posts = []
@@ -37,6 +39,13 @@ def listposts():
         post = posts[i]
         print(str(i)+" | "+post.account.acct)
     print("type id# to view a post")
+
+def refresh():
+    timeline = mastodon.timeline_home(limit=100)
+    posts = []
+    for post in timeline:
+        posts.append(post)
+
 
 def reply():
     rid = input("what is the the reply id of the post? ")
@@ -53,28 +62,33 @@ def view(post):
     print("----------------------------------")
     print("Created " + str(post.created_at))
     print("----------------------------------")
-    print(h.handle(post.content))
+    print(render(h.handle(post.content)))
     print("----------------------------------")
     print(str(post.reblogs_count) + " Reblogs")
     print(str(post.favourites_count) + " Likes")
     print("reply id: " + str(post.id))
     print("use command reply or r to reply to this post")
 
-
+print("------------------------------------")
+print("Welcome to mastoline! Mastodon on your terminal!")
+print("------------------------------------")
 while (True):
     user = mastodon.me()
     cmd = input(user.acct+" ~> ")
     
     if (cmd == "list") or (cmd == "l"):
         listposts()
+    elif (cmd == "refresh") or (cmd == "f"):
+        refresh()
     elif (cmd == "reply") or (cmd == "r"):
         reply()
     elif (cmd == "toot") or (cmd == "t"):
         post()
     elif (cmd == "help") or (cmd == "h"):
         print("NOTE: none of these commands have arguments")
-        print("list/l - lists the user's timeline")
-        print("view/v - shows a post's content from id")
+        print("# - view post of id")
+        print("list/l - lists the user's timeline with it's id next to it")
+        print("refresh/f - refreshes the timeline")
         print("reply/r - replies to a post from reply id")
         print("toot/t - posts a status on mastodon")
     elif (cmd == "exit") or (cmd == "e"):
